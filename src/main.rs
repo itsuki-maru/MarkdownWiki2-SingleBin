db7d98402b0e808caf5691100ccc341dab848de4
itsuki-maru <marumaru.developer@gmail.com>
2026-01-29 15:43:20 +0900

    Prepare for release v1.3.0

--- src/main.rs
+++ src/main.rs
@@ -127,7 +127,7 @@ struct Templates;
 async fn main() {
     // CLI定義
     let cli = Command::new("MarkdownWiki2")
-        .version("1.2.0")
+        .version("1.3.0")
         .author("Itsuki Maru")
         .about("MarkdownWiki2 Backend Server")
         .disable_help_flag(true)
